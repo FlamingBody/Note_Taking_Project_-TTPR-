3abcf431e53975794f0a0f776c3fe0aa27879a13
Chrisg3142 <130247951+Chrisg3142@users.noreply.github.com>
2025-06-30 13:51:03 -0400

    setting up files

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) =>{
+    res.render("index");
+});
+
+app.listen(port, (req, res)=>{
+    console.log(`running on port ${port}`);
+});
