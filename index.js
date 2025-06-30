f292d48cb35304731405c2cdc62bbf243b7732c4
Chrisg3142 <130247951+Chrisg3142@users.noreply.github.com>
2025-06-30 15:10:26 -0400

    adding styles and function

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+let notes = []; 
+
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -13,9 +15,24 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) =>{
-    res.render("index");
+    res.render("index", { notes });
 });
+ app.post("/new", (req, res) =>{
+    const {title, content} = req.body;
+    const newNote = {
+        id: Date.now(),
+        title, content,
+    };
+    notes.push(newNote);
+    res.redirect("/")
+})
 
+app.post("/delete/:id", (req, res) => {
+    const idToDelete = parseInt(req.params.id);
+    notes = notes.filter(note => note.id !== idToDelete);
+    res.redirect("/");
+  });
+  
 app.listen(port, (req, res)=>{
     console.log(`running on port ${port}`);
 });
